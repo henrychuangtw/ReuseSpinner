da9dcd927c0ca2817f61c0c97e9c5a0f31602c4d
henry <henry.chuang@allpay.com.tw>
2016-04-12 15:24:58 +0800

    no message

---
+++ app/src/main/java/tw/henrychuang/commonspinner/MainActivity.java
@@ -0,0 +1,108 @@
+package tw.henrychuang.commonspinner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
+import tw.henrychuang.lib.SpinnerActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private int mRequestCode_select_country = 1001;
+    private int mRequestCode_select_country_prompt = 1002;
+    private TextView mTextView_Country, mTextView_Country_prompt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView_Country = (TextView)findViewById(R.id.txt_country);
+        mTextView_Country.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] array = getResources().getStringArray(R.array.array_country);
+
+                ArrayList<Map.Entry<String,String>> arrayList = new ArrayList<>();
+                for (String str:array) {
+                    String[] ary_split = str.split(",");
+                    arrayList.add(new AbstractMap.SimpleEntry<>(ary_split[0], ary_split[1]));
+                }
+
+                Intent intent = new Intent(MainActivity.this, SpinnerActivity.class);
+                intent.putExtra(SpinnerActivity.Extra_Resource, arrayList);
+                startActivityForResult(intent, mRequestCode_select_country);
+
+            }
+        });
+
+        mTextView_Country_prompt = (TextView)findViewById(R.id.txt_country_prompt);
+        mTextView_Country_prompt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] array = getResources().getStringArray(R.array.array_country);
+
+                ArrayList<Map.Entry<String,String>> arrayList = new ArrayList<>();
+                for (String str:array) {
+                    String[] ary_split = str.split(",");
+                    arrayList.add(new AbstractMap.SimpleEntry<>(ary_split[0], ary_split[1]));
+                }
+
+                Intent intent = new Intent(MainActivity.this, SpinnerActivity.class);
+                intent.putExtra(SpinnerActivity.Extra_Resource, arrayList);
+                intent.putExtra(SpinnerActivity.Extra_SelectOne, getResources().getString(R.string.select_country_prompt));
+                startActivityForResult(intent, mRequestCode_select_country_prompt);
+            }
+        });
+
+    }
+
+    private void initListCountry(){
+        String[] array = getResources().getStringArray(R.array.array_country);
+
+        ArrayList<Map.Entry<String,String>> arrayList = new ArrayList<>();
+        for (String str:array) {
+            String[] ary_split = str.split(",");
+            arrayList.add(new AbstractMap.SimpleEntry<>(ary_split[0], ary_split[1]));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == mRequestCode_select_country && resultCode == RESULT_OK){
+            if(data != null){
+                Map.Entry<String,String> entry = (Map.Entry<String,String>) data.getSerializableExtra(SpinnerActivity.Result_Data);
+                if(entry != null){
+                    Log.i(TAG, String.format("get result -> key:%s , value:%s", entry.getKey(), entry.getValue()));
+                    mTextView_Country.setText(entry.getValue());
+
+                }
+
+            }
+
+        }else  if(requestCode == mRequestCode_select_country_prompt && resultCode == RESULT_OK){
+            if(data != null){
+                Map.Entry<String,String> entry = (Map.Entry<String,String>) data.getSerializableExtra(SpinnerActivity.Result_Data);
+                if(entry != null){
+                    Log.i(TAG, String.format("get result -> key:%s , value:%s", entry.getKey(), entry.getValue()));
+                    mTextView_Country_prompt.setText(entry.getValue());
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ lib_reusespinner/src/main/java/tw/henrychuang/lib/SpinnerActivity.java
@@ -0,0 +1,159 @@
+package tw.henrychuang.lib;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+/**
+ * Created by henry.chuang on 2016/4/7.
+ */
+public class SpinnerActivity extends Activity {
+    public static String Extra_Resource = "SpinnerActivity.Extra_Resource";
+    public static String Extra_SelectOne = "SpinnerActivity.Extra_SelectOne";
+    public static String Result_Data = "SpinnerActivity.Result_Data";
+
+    private String TAG = SpinnerActivity.class.getSimpleName();
+    private Context mContext;
+    private CustomSpinner mSpinner;
+    private ArrayAdapter<String> mArrayAdapter;
+    private ArrayList<Map.Entry<String,String>> mArrayList_resource;
+    private ArrayList<String> mArrayList_value;
+    private ArrayList<String> mArrayList_key;
+    private String mSelectOne;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.spinner_activity);
+
+        mSpinner = (CustomSpinner) findViewById(R.id.spinner_activity_spinner);
+        mContext = SpinnerActivity.this;
+
+        mArrayList_resource = (ArrayList<Map.Entry<String,String>>) getIntent().getSerializableExtra(Extra_Resource);
+        if(mArrayList_resource == null){
+            finish();
+        }
+
+        mSelectOne = getIntent().getExtras().getString(Extra_SelectOne, getResources().getString(R.string.str_select));
+        Log.i(TAG,"prompt string: " + mSelectOne);
+
+        mArrayList_value = new ArrayList<>();
+        mArrayList_key = new ArrayList<>();
+
+        mArrayList_key.add("-999");
+        mArrayList_value.add(mSelectOne);
+
+        for (int i = 0; i < mArrayList_resource.size(); i++){
+            Map.Entry<String, String> entry = mArrayList_resource.get(i);
+            Log.i(TAG, String.format("entry: %s , %s", entry.getKey(), entry.getValue()));
+            mArrayList_key.add(entry.getKey());
+            mArrayList_value.add(entry.getValue());
+        }
+
+
+        mArrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_option, mArrayList_value){
+            @Override
+            public boolean isEnabled(int position) {
+                // TODO Auto-generated method stub
+                if (position == 0) {
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+
+                TextView tv = (TextView) view;
+                if(position == 0) {
+                    Drawable drawable;
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        drawable = mContext.getDrawable(R.drawable.borderbottom);
+                    } else {
+                        drawable = mContext.getResources().getDrawable(R.drawable.borderbottom);
+                    }
+
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        tv.setBackground(drawable);
+                    } else {
+                        tv.setBackgroundDrawable(drawable);
+                    }
+
+                    tv.setTextColor(Color.GRAY);
+                }else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+        };
+
+
+        mSpinner.setAdapter(mArrayAdapter);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position != 0) {
+                    String key = mArrayList_key.get(position);
+                    String value = mArrayList_value.get(position);
+
+                    Log.i(TAG, String.format("select item: %s , %s", key, value));
+
+                    Intent intent = new Intent();
+                    intent.putExtra(Result_Data, new AbstractMap.SimpleEntry<>(key, value));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+        mSpinner.setSpinnerEventsListener(new CustomSpinner.OnSpinnerEventsListener() {
+            @Override
+            public void onSpinnerOpened() {
+
+
+            }
+
+            @Override
+            public void onSpinnerClosed() {
+                if(mSpinner.getSelectedItemPosition() == 0)
+                    finish();
+
+            }
+        });
+
+        mSpinner.performClick();
+
+    }
+
+
+}
